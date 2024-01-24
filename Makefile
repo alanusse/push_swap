81cc883e770c5371b232163347e57bbf59701265
Agustín Lanusse <lanusseagustin@gmail.com>
2024-01-24 12:26:07 +0100

    added all movements

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/06 13:43:05 by aglanuss          #+#    #+#              #
-#    Updated: 2024/01/23 12:59:28 by aglanuss         ###   ########.fr        #
+#    Updated: 2024/01/24 12:01:45 by aglanuss         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,9 +19,9 @@ RM = rm -rf
 
 INCLUDES = push_swap.h utils.h libft.h
 
-SRCS = push_swap.c print_error.c lists.c check_arguments.c a_movements.c\
-			 b_movements.c other_movements.c is_sorted.c ft_atoi.c ft_atol.c\
-			 ft_isdigit.c ft_strchr.c ft_strcmp.c ft_strlen.c
+SRCS = push_swap.c print_error.c lists.c check_arguments.c push.c rotate.c\
+			 rev_rotate.c swap.c is_sorted.c ft_atoi.c ft_atol.c ft_isdigit.c\
+			 ft_strchr.c ft_strcmp.c ft_strlen.c
 
 OBJS = $(SRCS:.c=.o)
 
---
+++ rotate.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 11:51:39 by aglanuss          #+#    #+#             */
+/*   Updated: 2024/01/24 12:22:55 by aglanuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "movements.h"
+
+static void  reverse(t_list **stack)
+{
+  t_list  *current;
+  t_list  *first;
+
+  if (lstsize(stack) < 2)
+    return ;
+  first = *stack;
+  *stack = first->next;
+  first->next = NULL;
+  current = *stack;
+  while (current->next)
+    current = current->next;
+  current->next = first;
+}
+
+void  ra(t_list **stack_a)
+{
+  reverse(stack_a);
+  write(1, "ra\n", 3);
+}
+
+void  rb(t_list **stack_b)
+{
+  reverse(stack_b);
+  write(1, "rb\n", 3);
+}
+void  rr(t_list **stack_a, t_list **stack_b)
+{
+  reverse(stack_a);
+  reverse(stack_b);
+  write(1, "rr\n", 3);
+}
