79bfd8fa6d67b3440dfb4845ae2aeb10a3bcc9b8
Agustin Lanusse <lanusseagustin@gmail.com>
2024-02-04 01:53:36 +0100

    modified sort four numbers function

--- sort_four_numbers.c
+++ sort_four_numbers.c
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 18:14:06 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/01/31 23:52:31 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/02/04 00:52:15 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,48 @@
 /**
  * Find index of biggest number of the stack.
 */
-static int  get_idx_biggest_num(t_list **stack_a)
+static int  get_idx_smallest_num(t_list **stack_a)
 {
   t_list  *current;
-  int     biggest_idx;
+  int     smallest_idx;
   int     i;
 
-  biggest_idx = 0;
+  smallest_idx = 0;
   current = *stack_a;
   i = 0;
   while (current)
   {
-    if (current->content > get_lst_content_by_idx(stack_a, biggest_idx))
-      biggest_idx = i;
+    if (current->content < get_lst_content_by_idx(stack_a, smallest_idx))
+      smallest_idx = i;
     current = current->next;
     i++;
   }
-  return (biggest_idx);
+  return (smallest_idx);
 }
 
 void  sort_four_numbers(t_list **stack_a, t_list **stack_b)
 {
-  int biggest_num_idx;
+  int smallest_num_idx;
 
-  biggest_num_idx = get_idx_biggest_num(stack_a);
-  if (biggest_num_idx == 0)
+  smallest_num_idx = get_idx_smallest_num(stack_a);
+  if (smallest_num_idx == 0)
     pb(stack_a, stack_b);
-  else if (biggest_num_idx == 1)
+  else if (smallest_num_idx == 1)
   {
     sa(stack_a);
     pb(stack_a, stack_b);
   }
-  else if (biggest_num_idx == 2)
+  else if (smallest_num_idx == 2)
   {
     rra(stack_a);
     rra(stack_a);
     pb(stack_a, stack_b);
   }
-  else if (biggest_num_idx == 3)
+  else if (smallest_num_idx == 3)
   {
     rra(stack_a);
     pb(stack_a, stack_b);
   }
   sort_three_numbers(stack_a);
   pa(stack_a, stack_b);
-  ra(stack_a);
 }
