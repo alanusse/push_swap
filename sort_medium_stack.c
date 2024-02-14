ee769a5c8c986dca1dd25e541c056bb95c781661
Agustin Lanusse <lanusseagustin@gmail.com>
2024-02-14 13:57:54 +0100

    ea

--- sort_medium_stack.c
+++ sort_medium_stack.c
@@ -6,30 +6,31 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 09:56:43 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/02/11 22:10:08 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/02/14 13:56:58 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithms.h"
 
-
-
 void  sort_medium_stack(t_list **stack_a)
 {
   int chunk;
   int num;
+  int num_limit;
   int lst_size;
 
   lst_size = lstsize(stack_a);
   chunk = 1;
   while (chunk <= 5)
   {
-    num = (lst_size / 5) * chunk;
+    num = (lst_size / 5) * chunk - (lst_size / 5);
+    num_limit = (lst_size / 5) * chunk;
     if (chunk == 5)
-      num += lst_size % 5;
-    while (num > num - (lst_size / 5))
+      num_limit += (lst_size % 5);
+    while (num < num_limit)
     {
-      num--;
+      // TODO: check cost of movement and do it
+      num++;
     }
     chunk++;
   }
