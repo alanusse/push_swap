89432b893a1936f91ac685cd66ebd952a787dd5b
Agustín Lanusse <lanusseagustin@gmail.com>
2024-03-22 00:33:50 +0100

    sort medium stack

--- src/sort_algorithms/sort_medium_stack.c
+++ src/sort_algorithms/sort_medium_stack.c
@@ -6,32 +6,69 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 09:56:43 by aglanuss          #+#    #+#             */
-/*   Updated: 2024/03/11 23:45:40 by aglanuss         ###   ########.fr       */
+/*   Updated: 2024/03/22 00:33:11 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/movements.h"
 
-void  sort_medium_stack(t_list **stack_a)
+int	contain_chunk_numbers(t_list **stack, int chunk, int chunk_size)
 {
-  int chunk;
-  int num;
-  int num_limit;
-  int lst_size;
+	t_list	*curr;
+	int			chunk_min;
+	int			chunk_max;
 
-  lst_size = lstsize(stack_a);
-  chunk = 1;
-  while (chunk <= 5)
-  {
-    num = (lst_size / 5) * chunk - (lst_size / 5);
-    num_limit = (lst_size / 5) * chunk;
-    if (chunk == 5)
-      num_limit += (lst_size % 5);
-    while (num < num_limit)
-    {
-      // TODO: check cost of movement and do it
-      num++;
-    }
-    chunk++;
-  }
+	curr = *stack;
+	chunk_min = chunk * chunk_size - chunk_size;
+	chunk_max = chunk * chunk_size - 1;
+	while (curr)
+	{
+		if (curr->idx >= chunk_min && curr->idx <= chunk_max)
+			return (1);
+		curr = curr->next;
+	}
+	return (0);
+}
+
+void	move_chunk_numbers(t_list **stack_a, int chunk, int chunk_size)
+{
+	int			first_num;
+	int			second_num;
+	int			chunk_min;
+	int			chunk_max;
+	int			i;
+	t_list	*curr;
+
+	first_num = -1;
+	second_num = -1;
+	chunk_min = chunk * chunk_size - chunk_size;
+	chunk_max = chunk * chunk_size - 1;
+	i = 0;
+	curr = *stack_a;
+	while (first_num == -1 && curr)
+	{
+		if (curr->idx >= chunk_min && curr->idx <= chunk_max)
+			first_num = i;
+		curr = curr->next;
+		i++;
+	}
+}
+
+void	chunks_sort(int chunks, t_list **stack_a)
+{
+	int chunk_size;
+	int current_chunk;
+
+	chunk_size = (lstsize(stack_a) / chunks);
+	current_chunk = 1;
+	while (current_chunk <= chunks)
+	{
+		if (current_chunk == chunks)
+			chunk_size += lstsize(stack_a) % chunks;
+		while (contain_chunk_numbers(stack_a, current_chunk, chunk_size))
+		{
+			// check cost and do it
+		}
+		current_chunk++;
+	}
 }
